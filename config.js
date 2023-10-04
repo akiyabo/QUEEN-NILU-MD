71e4814070bd5a77df6b78f2613b3d3e1fb825a3
akiyabo <146976990+akiyabo@users.noreply.github.com>
2023-10-04 12:49:18 -0700

    Update config.js

--- config.js
+++ config.js
@@ -14,18 +14,18 @@ function convertToBool(text, fault = 'true') {
 
 // PUT YOUR SESSION ID HERE  ##Required 
 
-global.SESSION_ID = '' // PUT your session ID BY scan QR 
+global.SESSION_ID = 'QueenNilu;;;N3IAGB6Q#do5qv-NY7ilzJEkfyjP8iKcYn33Au-hA0JI0hX4X4mA' // PUT your session ID BY scan QR 
 
 // databse URL  ##Required 
-global.POSTGRESQL_URL = ''
+global.POSTGRESQL_URL = 'postgres://akiaki_user:9EZswtVdBqf6NDzM3YYpEBL6eBzH3UQl@dpg-ckeonsdtj22c738tjg00-a/akiaki'
 
 
 
 // ⚙️ OWNER SETTINGS  ⚙️
 
-global.OWNER_NUMBER = '94767438882'
+global.OWNER_NUMBER = '94779848097'
 
-global.OWNER_NAME = 'Janiya_X'
+global.OWNER_NAME = 'snoop_X'
 
 global.TIMEZONE = 'Asia/Colombo' 
 
